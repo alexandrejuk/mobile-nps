b52e499f0b23f23a86edde39f63e86dd897af774
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-23 07:23:07 -0300

    chore: update card action

--- src/stories/components/Card/index.js
+++ src/stories/components/Card/index.js
@@ -7,7 +7,7 @@ export default {
 }
 
 const props = {
-  action: action('clicked'),
+  action: action('card action!'),
   image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
   title: 'A8/60',
 }
