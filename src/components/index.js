ebdd8a623f813b4032cc1a8b577f82eef556dc06
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-23 06:59:30 -0300

    components: add collapse component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Button from './Button'
 import Card from './Card'
+import Collapse from './Collapse'
 import Modal from './Modal'
 import Title from './Title'
 
 export {
   Button,
   Card,
+  Collapse,
   Modal,
   Title,
 }
