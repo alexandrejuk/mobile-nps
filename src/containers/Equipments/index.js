a04011a18f82e31eee05cdf997a0262cb4f4fee1
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-25 18:59:46 -0300

    containers: update equipments containers

--- src/containers/Equipments/index.js
+++ src/containers/Equipments/index.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Equipments = ( ) => {
-    return (
-        <h1>Test</h1>
-    )
+import CompanyLogo from '../../assets/images/logo.svg'
+import { Card } from '../../components'
+import styles from './style.module.css'
+
+const Equipments = ({
+  equipments,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={CompanyLogo} alt="company logo" />
+      </div>
+      <div className={styles.equipments}>
+        {equipments.map(({
+          action,
+          id,
+          image,
+          title,
+        }) => (
+          <div
+            key={id}
+            className={styles.equipmentItem}
+          >
+            <Card
+              action={action}
+              image={image}
+              title={title}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default Equipments 
+Equipments.propTypes = {
+  goBack: PropTypes.func.isRequired,
+  equipments: PropTypes.shape({
+    action: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+export default Equipments
--- src/stories/containers/Equipments/index.js
+++ src/stories/containers/Equipments/index.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
 import Equipments from '../../../containers/Equipments'
 
 export default {
   title: 'Containers/Equipments',
 }
 
+const equipments = [
+  {
+    action: action('card action!'),
+    id: 1,
+    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
+    title: 'A8/60',
+  },
+  {
+    action: action('card action!'),
+    id: 2,
+    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
+    title: 'A8/60',
+  },
+  {
+    action: action('card action!'),
+    id: 3,
+    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
+    title: 'A8/60',
+  },
+  {
+    action: action('card action!'),
+    id: 4,
+    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
+    title: 'A8/60',
+  },
+  {
+    action: action('card action!'),
+    id: 5,
+    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
+    title: 'A8/60',
+  },
+]
+
 export const Default = () => (
-  <Equipments />
+  <Equipments
+    equipments={equipments}
+  />
 )
