1fac2635951389b35e17164f73810604f8044a55
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-02-25 21:09:46 -0300

    pages: add login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router'
+import { isEmpty } from 'ramda'
+
 import LoginContainer from '../../containers/Login'
 
-const Login = () => (
-  <LoginContainer />
-)
+const Login = ({
+  history,
+}) => {
+  const access = 2102
+  const [data, setData] = useState({
+    code_access: '',
+  })
 
-export default Login
+  const handleRedirect = (codeAccess) => {
+    const { code_access } = codeAccess
+    if (access === Number(code_access)) {
+      localStorage.setItem('code_access', code_access)
+      return history.push('/logged/equipments')
+    }
+    return null
+  }
+
+  const handleChange = values => setData(values)
+
+  const handleSubmit = (values, formErrors = {}) => {
+    return (
+      isEmpty(formErrors)
+      && handleRedirect(values)
+    )
+  }
+
+  return (
+    <LoginContainer
+      data={data}
+      onChange={handleChange}
+      onSubmit={handleSubmit}
+    />
+  )
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+}
+
+export default withRouter(Login)
