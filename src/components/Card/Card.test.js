ae7396d0882696a08a8f302621d3a390bc19bddd
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-08 12:16:08 -0300

    chore: fix a bug on test

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react'
 import Card from './'
 
 const props = {
-  action: null,
+  action: () => {},
   image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
   title: 'A8/60',
 }
