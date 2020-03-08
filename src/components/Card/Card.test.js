1e2c2a97ce40bc7f41545e917e80706418b25d52
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-08 12:15:34 -0300

    chore: add card test snapshot

---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Card from './'
+
+const props = {
+  action: null,
+  image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
+  title: 'A8/60',
+}
+
+
+describe('Card component', () => {
+  it('should render correctly', () => {
+    const { container } = render(
+      <Card {...props} />
+    )
+    expect(container).toMatchSnapshot()
+  })
+})
