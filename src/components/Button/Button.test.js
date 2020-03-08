151d2a1e37af3714672fee376c93bb79ae015b2e
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-08 12:35:18 -0300

    chore: add snapshot to outline button

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -4,10 +4,21 @@ import { render } from '@testing-library/react'
 import Button from './'
 
 describe('Button component', () => {
-  it('should render correctly', () => {
+  it('should render correctly primary button', () => {
     const { container } = render(
       <Button
         color="primary"
+      >
+        Button component
+      </Button>
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly outline button', () => {
+    const { container } = render(
+      <Button
+        color="outline"
       >
         Button component
       </Button>
