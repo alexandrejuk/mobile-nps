812feb05b95deda4b4c222370a49d88235fc145f
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-09 12:33:56 -0300

    chore: update test

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -15,18 +15,21 @@ const props = {
 describe('Card component', () => {
   it('should render correctly', () => {
   const callback = jest.fn()
-    const { container, getByText } = render(
+    const { container, getByText, getByAltText } = render(
       <Card
         {...props}
         action={callback}
       />
     )
 
+    const imageCard = getByAltText("item card")
     const SampleCard = getByText(props.title)
     expect(callback).not.toHaveBeenCalled()
 
     fireEvent.click(SampleCard)
 
+    expect(SampleCard.innerHTML).toEqual(props.title)
+    expect(imageCard.getAttribute("src")).toEqual(props.image)
     expect(callback).toHaveBeenCalled()
     expect(container).toMatchSnapshot()
   })
