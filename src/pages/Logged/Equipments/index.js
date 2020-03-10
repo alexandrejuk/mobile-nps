9865c3496958b1cbd7a5208106738362ef15875f
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-10 00:21:14 -0300

    chore: add mock

--- src/pages/Logged/Equipments/index.js
+++ src/pages/Logged/Equipments/index.js
@@ -2,34 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router'
 import EquipmentsContainer from '../../../containers/Equipments'
-
-const equipments = [
-  {
-    id: 1,
-    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
-    title: 'A8/60',
-  },
-  {
-    id: 2,
-    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
-    title: 'A8/60',
-  },
-  {
-    id: 3,
-    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
-    title: 'A8/60',
-  },
-  {
-    id: 4,
-    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
-    title: 'A8/60',
-  },
-  {
-    id: 5,
-    image: 'https://www.nestleprofessional.com.br/sites/g/files/gfb331/f/styles/product/public/media/maquina-alegria-8x60-2.png?itok=3w8wjKl9',
-    title: 'A8/60',
-  },
-]
+import EquipmentsMock from '../../../utils/api/mocks'
 
 const Equipments = ({
   history,
@@ -38,7 +11,7 @@ const Equipments = ({
   return (
     <EquipmentsContainer
       action={goToDetail}
-      equipments={equipments}
+      equipments={EquipmentsMock}
     />
   )
 }
