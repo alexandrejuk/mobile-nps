aecfc50295519422a7005f83dfc9f96cd8b4068a
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-10 00:20:43 -0300

    chore: add mocks and new images

---
+++ src/utils/api/mocks.js
@@ -0,0 +1,310 @@
+const EquipmentsMock = [
+  {
+      "id": 1,
+      "description": "Com a máquina NESCAFÉ® Alegria, você oferece variedade de cappuccinos e achocolatados para o seu negócio. É perfeita para quem quer qualidade, praticidade e padronização, pois a máquina conta com 8 opções de bebidas saborosas, cremosas e exclusivas da Nestlé®.",
+      "name": "Prima",
+      "height": "65,0 cm",
+      "image": 'PrimaPremix',
+      "width": "37,0 cm",
+      "deep": "44,0 cm",
+      "weight": "34,0 Kg",
+      "voltage": "1600W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "AO 140ml LP",
+              "src": null
+          },
+          {
+              "title": "AO 90ml LP",
+              "src": null
+          },
+          {
+              "title": "AP 140ml LP",
+              "src": null
+          },
+          {
+              "title": "AP 90ml LP",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 2,
+      "description": "Com a máquina NESCAFÉ® Alegria, você oferece variedade de cappuccinos e achocolatados para o seu negócio. É perfeita para quem quer qualidade, praticidade e padronização, pois a máquina conta com 8 opções de bebidas saborosas, cremosas e exclusivas da Nestlé®.",
+      "name": "Lioness",
+      "height": "50,0 cm",
+      "image": 'Lioness',
+      "width": "37,0 cm",
+      "deep": "45,0 cm",
+      "weight": "27,0 Kg",
+      "voltage": "1600W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "AO 140ml LP",
+              "src": null
+          },
+          {
+              "title": "AO 90ml LP",
+              "src": null
+          },
+          {
+              "title": "AP 140ml LP",
+              "src": null
+          },
+          {
+              "title": "AP 90ml LP",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 3,
+      "description": "Com a máquina NESCAFÉ® Alegria, você oferece variedade de cappuccinos e achocolatados para o seu negócio. É perfeita para quem quer qualidade, praticidade e padronização, pois a máquina conta com 6 opções de bebidas saborosas, cremosas e exclusivas da Nestlé®.",
+      "name": "A6/30",
+      "height": "56,0 cm",
+      "image": 'A630',
+      "width": "31,5 cm",
+      "deep": "49,0 cm",
+      "weight": "20,4 Kg",
+      "voltage": "1300W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "X 140ml",
+              "src": null
+          },
+          {
+              "title": "X 90ml",
+              "src": null
+          },
+          {
+              "title": "AM 140ml",
+              "src": null
+          },
+          {
+              "title": "AM 90ml",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 4,
+      "description": "Com a máquina NESCAFÉ® Alegria, você oferece variedade de cappuccinos e achocolatados para o seu negócio. É perfeita para quem quer qualidade, praticidade e padronização, pois a máquina conta com 8 opções de bebidas saborosas, cremosas e exclusivas da Nestlé®.",
+      "name": "A8/60",
+      "height": "61,5 cm",
+      "image": 'A860',
+      "width": "46,0 cm",
+      "deep": "62,0 cm",
+      "weight": "35,0 Kg",
+      "voltage": "1600W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "AO 140ml",
+              "src": null
+          },
+          {
+              "title": "AO 90ml",
+              "src": null
+          },
+          {
+              "title": "AP 140ml",
+              "src": null
+          },
+          {
+              "title": "AP 90ml",
+              "src": null
+          },
+          {
+              "title": "C018",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 5,
+      "description": "NESCAFÉ Milano – A bebida premium perfeita para aqueles momentos especiais. Surpreenda-se com NESCAFÉ® Milano, uma solução que entrega bebidas premium em poucos segundos, com padronização e qualidade garantidas.",
+      "name": "M8/60",
+      "height": "66,0 cm",
+      "image": 'M860',
+      "width": "46,0 cm",
+      "deep": "62,0 cm",
+      "weight": "35,8 Kg",
+      "voltage": "1600W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "B M860",
+              "src": null
+          },
+          {
+              "title": "E M860",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 6,
+      "description": "Exclusividade :Máquina italiana da mais alta qualidade, com o novo café 100% arábica torrado e moido na hora. Rendimento: Consumo total de seus insumos, sem descarte ou desperdício. Eficiência: Muito mais agilidade no preparo Economia: Não tem caldeira, por isso consome 90% menos energia que as máquinas do mercado. Máquinas em comodato e assistência técnica gratuita. Marcas Exclusivas: Uma perfeita solução com 12 opções de bebidas, todas com marcas que só a Nestlé pode oferecer",
+      "name": "ATP60E",
+      "height": "66,6 cm",
+      "image": 'ATP60E',
+      "width": "42,2 cm",
+      "deep": "59,9 cm",
+      "weight": "35,2 Kg",
+      "voltage": "3000W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "AI 140ml",
+              "src": null
+          },
+          {
+              "title": "AI 90ml",
+              "src": null
+          },
+          {
+              "title": "C001",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 7,
+      "description": "Exclusividade :Máquina italiana da mais alta qualidade, com o novo café 100% arábica torrado e moido na hora. Rendimento: Consumo total de seus insumos, sem descarte ou desperdício. Eficiência: Muito mais agilidade no preparo Economia: Não tem caldeira, por isso consome 90% menos energia que as máquinas do mercado. Máquinas em comodato e assistência técnica gratuita. Marcas Exclusivas: Uma perfeita solução com 12 opções de bebidas, todas com marcas que só a Nestlé pode oferecer",
+      "name": "MTS60E",
+      "height": "66,6 cm",
+      "image": 'MTS60E',
+      "width": "42,2 cm",
+      "deep": "59,9 cm",
+      "weight": "35,2 Kg",
+      "voltage": "3000W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "H",
+              "src": null
+          },
+          {
+              "title": "H Menu 2",
+              "src": null
+          },
+          {
+              "title": "F",
+              "src": null
+          },
+          {
+              "title": "I",
+              "src": null
+          },
+          {
+              "title": "G",
+              "src": null
+          },
+          {
+              "title": "J",
+              "src": null
+          },
+          {
+              "title": "K",
+              "src": null
+          },
+          {
+              "title": "C009",
+              "src": null
+          },
+          {
+              "title": "C009 Menu 2",
+              "src": null
+          },
+          {
+              "title": "C010",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 8,
+      "description": "NESCAFÉ® Alegria - A maior variedade de cappuccinos e achocolatados para esquentar o seu negócio! Conheça essa solução completa, que entrega produtos de qualidade com marcas conhecidas e oferece ao seu negócio praticidade e padronização.",
+      "name": "FTP60E",
+      "height": "67,2 cm",
+      "image": 'FTP60E',
+      "width": "42,2 cm",
+      "deep": "56,8 cm",
+      "weight": "36,0 Kg",
+      "voltage": "3000W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "AI 140ml",
+              "src": null
+          },
+          {
+              "title": "AI 90ml",
+              "src": null
+          },
+          {
+              "title": "C001",
+              "src": null
+          }
+      ]
+  },
+  {
+      "id": 9,
+      "description": "NESCAFÉ – A bebida premium perfeita para aqueles momentos especiais. Surpreenda-se com NESCAFÉ® Milano, uma solução que entrega bebidas premium em poucos segundos, com padronização e qualidade garantidas.",
+      "name": "FTS60E",
+      "height": "67,2 cm",
+      "image": 'FTS60E',
+      "width": "42,2 cm",
+      "deep": "56,8 cm",
+      "weight": "36,0 Kg",
+      "voltage": "3000W",
+      "tension": "220V",
+      "revenues": [
+          {
+              "title": "H",
+              "src": null
+          },
+          {
+              "title": "H Menu 2",
+              "src": null
+          },
+          {
+              "title": "F",
+              "src": null
+          },
+          {
+              "title": "I",
+              "src": null
+          },
+          {
+              "title": "G",
+              "src": null
+          },
+          {
+              "title": "J",
+              "src": null
+          },
+          {
+              "title": "K",
+              "src": null
+          },
+          {
+              "title": "C009",
+              "src": null
+          },
+          {
+              "title": "C009 Menu 2",
+              "src": null
+          },
+          {
+              "title": "C010",
+              "src": null
+          }
+      ]
+  }
+]
+
+export default EquipmentsMock
