e6242d2a61dd944929632df63f4db0fd34f131d6
gabrieloliveira524@gmail.com <61363201+GEOliveira1@users.noreply.github.com>
2020-03-14 12:17:29 -0300

    fix(react-ga): update initialize

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
+import ReactGA from 'react-ga'
+
 import './index.css'
 import Routes from './routes'
 import * as serviceWorker from './serviceWorker'
+
+ReactGA.initialize('UA-160310065-1')
 
 ReactDOM.render(
   <HashRouter>
