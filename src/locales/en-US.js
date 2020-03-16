0d12fcb825299ed402430769379154d87f94a7db
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-16 17:25:38 -0300

    chore:  translate detail container

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -2,7 +2,20 @@ const en = {
   login: {
     accessCode: 'Access code',
     buttonTitle: 'Sign Up',
-  }
+  },
+  detail: {
+    dimension: 'Dimension',
+    description: 'Description',
+    deep: 'D',
+    errors: 'Errors',
+    height: 'H',
+    problem: 'Problem',
+    potencyTension: 'Potency / Tension',
+    solution: 'Solution',
+    revenues: 'Revenues',
+    width: 'W',
+    weight: 'Weight',
+  },
 }
 
 export default en
--- src/locales/pt-BR.js
+++ src/locales/pt-BR.js
@@ -2,7 +2,20 @@ const pt = {
   login: {
     accessCode: 'Código de acesso',
     buttonTitle: 'Entrar',
-  }
+  },
+  detail: {
+    dimension: 'Dimensões',
+    description: 'Descrição',
+    deep: 'p',
+    errors: 'Erros',
+    height: 'A',
+    problem: 'Problema',
+    potencyTension: 'Potência / Tensão',
+    solution: 'Solução',
+    revenues: 'Receitas',
+    width: 'L',
+    weight: 'Peso',
+  },
 }
 
 export default pt
