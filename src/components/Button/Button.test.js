b8bf820eb2ff204163797f420dbbb411bab09a2c
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-03-21 02:57:11 -0300

    chore: update button component

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -29,12 +29,158 @@ describe('Button component', () => {
     expect(container).toMatchSnapshot()
   })
 
+  it('should render correctly secondary button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="secondary"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
   it('should render correctly outline button', () => {
     const callback = jest.fn()
     const { container, getByText } = render(
       <Button
         action={callback}
         color="outline"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly small primary button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="primary"
+        size="small"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly small secondary button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="secondary"
+        size="small"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly small outline button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="outline"
+        size="small"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly xsmall primary button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="primary"
+        size="xsmall"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly xsmall secondary button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="secondary"
+        size="xsmall"
+      >
+        {BUTTON_TEXT}
+      </Button>
+    )
+    const SampleButton = getByText(BUTTON_TEXT)
+
+    expect(callback).not.toHaveBeenCalled()
+
+    fireEvent.click(SampleButton)
+
+    expect(callback).toHaveBeenCalled()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render correctly xsmall outline button', () => {
+    const callback = jest.fn()
+    const { container, getByText } = render(
+      <Button
+        action={callback}
+        color="outline"
+        size="xsmall"
       >
         {BUTTON_TEXT}
       </Button>
