4a299872958d536e363e4bd601331fdfee666d85
gabrieloliveira524@gmail.com <61363201+GEOliveira1@users.noreply.github.com>
2020-03-21 11:57:41 -0300

    fix: remove collapse component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Button from './Button'
 import Card from './Card'
-import Collapse from './Collapse'
 import Header from './Header'
 import Input from './Input'
 import Confirm from './Confirm'
@@ -10,7 +9,6 @@ import Title from './Title'
 export {
   Button,
   Card,
-  Collapse,
   Confirm,
   Header,
   Input,
