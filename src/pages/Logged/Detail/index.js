e95ce431bb84901437a8e9548aee59996baeba7d
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-03-22 00:41:20 -0300

    Revert "chore: update equipment detail container"
    This reverts commit 04bb7632e5759a592def1863a782f88bfb67d000.

--- src/pages/Logged/Detail/index.js
+++ src/pages/Logged/Detail/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router'
 import DetailContainer from '../../../containers/Detail'
@@ -10,29 +10,14 @@ const Detail = ({
   history,
   match
 }) => {
-  const [showModal, setShowModal] = useState(false)
-  const [issue, setIssue] = useState({})
-
   const goBack = () => history.goBack()
   const { id } = match.params
   const product = EquipmentsMock.find(product => product.id === Number(id))
   GAInitialize(`/logged/detail/${id}`)
-
-  const handleShowModal = issue => {
-    setIssue(issue)
-    setShowModal(true)
-  }
-
-  const handleCloseModal = () => setShowModal(false)
-
   return (
     <DetailContainer
       goBack={goBack}
       product={product || {}}
-      showModal={showModal}
-      onOpenModal={handleShowModal}
-      issueSelected={issue}
-      onCloseModal={handleCloseModal}
     />
   )
 }
