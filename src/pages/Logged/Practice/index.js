47b64d4d37e4c3bba820502855c9be759bd6079f
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-03-22 00:41:20 -0300

    Revert "fix: some bug"
    This reverts commit 06373cb3e29b79d2edee3360dc3af1af61051e92.

--- src/pages/Logged/Practice/index.js
+++ src/pages/Logged/Practice/index.js
@@ -14,29 +14,26 @@ const progressValue = (steps, step, type) => (
 
 const Practice = ({
   history,
-  match,
 }) => {
   GAInitialize('/logged/practice-detail')
 
-  const { id } = match.params
-  const { practices } = goodPractices.find(practice => practice.id === Number(id))
   const [step, setStep] = useState(0)
   const [progress, setProgress] = useState(
-    progressValue(practices.length, step, 'increment')
+    progressValue(goodPractices.length, step, 'increment')
   )
 
   const goBack = () => history.push('/logged/equipments')
 
   const handleNext = () => {
-    const incrementProgress = progressValue(practices.length, step, 'increment')
-    if(step < (practices.length - 1)) {
+    const incrementProgress = progressValue(goodPractices.length, step, 'increment')
+    if(step < (goodPractices.length - 1)) {
       setProgress(incrementProgress)
       setStep(step + 1)
     }
   }
 
   const handlePrev =  () => {
-    const decrementProgress = progressValue(practices.length, step, 'decrement')
+    const decrementProgress = progressValue(goodPractices.length, step, 'decrement')
     if(step > 0) {
       setProgress(decrementProgress)
       setStep(step - 1)
@@ -44,7 +41,7 @@ const Practice = ({
   }
 
   const actionNext = (
-    step === (practices.length - 1)
+    step === (goodPractices.length - 1)
     ? goBack
     : handleNext
   )
@@ -54,9 +51,9 @@ const Practice = ({
       close={goBack}
       onStep={actionNext}
       onPrevStep={handlePrev}
-      practice={practices[step]}
+      practice={goodPractices[step]}
       step={step}
-      steps={(practices.length - 1)}
+      steps={(goodPractices.length - 1)}
       progress={progress}
     />
   )
@@ -66,14 +63,6 @@ Practice.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-      ]).isRequired,
-    }).isRequired,
-  }).isRequired
 }
 
 export default withRouter(Practice)
