e70045d8d09e2641c02f77c61db0ca12fb9c49a5
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-03-22 00:41:20 -0300

    Revert "chore: update equipment detail container"
    This reverts commit 04bb7632e5759a592def1863a782f88bfb67d000.

--- src/containers/Detail/index.js
+++ src/containers/Detail/index.js
@@ -3,20 +3,11 @@ import PropTypes from 'prop-types'
 import { map, prop } from 'ramda'
 import Lightbox from 'react-image-lightbox'
 import 'react-image-lightbox/style.css'
-
 import Translate from '../../locales'
 
-import ScaleWidthSvg from '../../assets/icons/scale-width.svg'
-import ScaleHeightSvg from '../../assets/icons/scale-height.svg'
-import ScaleDeepSvg from '../../assets/icons/scale-deep.svg'
-import VoltstSvg from '../../assets/icons/volts.svg'
-import weightSvg from '../../assets/icons/weight.svg'
-import CloseSvg from '../../assets/icons/close.svg'
-import ArrowSvg from '../../assets/icons/arrow-left.svg'
-
 import {
-  Button,
-  Modal,
+  Collapse,
+  Header,
   Title,
 } from '../../components'
 
@@ -27,243 +18,199 @@ import {
   imagesProduct,
 } from './images'
 
+const issue = ({
+  id,
+  name,
+  problems,
+  resolves,
+}) => (
+  <Collapse key={id} title={name}>
+    <div className={styles.issueSection}>
+      <Title
+        size="small"
+        text={Translate('detail.problem')}
+        type="bold"
+      />
+    {
+      problems && map((problem, index) =>(
+        <Title
+          key={`${index}${problem}`}
+          size="small"
+          text={problem}
+        />
+      ), problems)
+    }
+    </div>
+    <div className={styles.issueSection}>
+      <Title
+        size="small"
+        text={Translate('detail.solution')}
+        type="bold"
+      />
+    {
+      resolves && map((resolve, index) => (
+        <Title
+          key={`${index}${resolve}`}
+          size="small"
+          text={resolve}
+        />
+      ), resolves)
+    }
+     </div>
+  </Collapse>
+)
+
 const Detail = ({
-  issueSelected,
-  onCloseModal,
-  onOpenModal,
   product = {},
-  showModal,
   goBack,
 }) => {
   const [isOpen, setIsOpen] = useState(false)
   const [photoIndex, setPhotoIndex] = useState(0)
   const revenues = prop('revenues', product)
   const images = map(({ src }) => (revenuesImage[src]), revenues)
-  const issues = prop('issues', product)
 
   const handleRevenue = (index) => {
     setPhotoIndex(index)
     setIsOpen(true)
   }
 
-  const issue = issueItem => (
-    <div
-      className={styles.issueItem}
-      key={issueItem.id}
-    >
-      <Button
-        action={() => onOpenModal(issueItem)}
-        color="secondary"
-        size="xsmall"
-      >
-        {issueItem.name}
-      </Button>
-    </div>
-  )
-
   const revenue = ({
+    src,
     title,
   }, index) => (
     <div
       className={styles.revenueItem}
       key={index}
+      onClick={() => handleRevenue(index)}
     >
-      <Button
-        action={() => handleRevenue(index)}
-        color="outline"
+      <Title
         size="small"
-      >
-        {title}
-      </Button>
+        text={`Cod: ${title}`}
+        type="bold"
+      />
+      <div className={styles.revenuesAvatar}>
+        <img src={revenuesImage[src]} alt="revenue gallery" />
+      </div>
     </div>
   )
-
   return (
     <div className={styles.container}>
-      <div
-        onClick={goBack}
-        role="button"
-      >
-        <img src={ArrowSvg} alt="go back" />
-      </div>
-      <div className={styles.productImage}>
-        <img src={imagesProduct[prop('image', product)]} alt="product" />
-      </div>
-      <Title
-        size="large"
-        type="bold"
-        text={prop('name', product)}
+      <Header
+        action={goBack}
       />
-      <Title
-        size="medium"
-        type="bold"
-        text={Translate('detail.description')}
-      />
-      <Title
-        color="secondary"
-        size="normal"
-        text={prop('description', product)}
-      />
-      <div className={styles.equipmentInfoContainer}>
-        <div className={styles.equipmentInfo}>
-          <img src={ScaleWidthSvg} alt="scale product" />
-          <Title
-            size="small"
-            type="bold"
-            textAlign="center"
-            color="cardinal"
-            text={`${Translate('detail.width')} ${prop('width', product)}`}
-          />
-        </div>
-        <div className={styles.equipmentInfo}>
-          <img src={ScaleHeightSvg} alt="scale product" />
-          <Title
-            size="small"
-            type="bold"
-            textAlign="center"
-            color="cardinal"
-            text={`${Translate('detail.height')} ${prop('height', product)}`}
-          />
-        </div>
-        <div className={styles.equipmentInfo}>
-          <img src={ScaleDeepSvg} alt="scale product" />
-          <Title
-            size="small"
-            type="bold"
-            textAlign="center"
-            color="cardinal"
-            text={`${Translate('detail.deep')} ${prop('deep', product)}`}
-          />
-        </div>
-        <div className={styles.equipmentInfo}>
-          <img src={VoltstSvg} alt="volts product" />
-          <Title
-            size="small"
-            type="bold"
-            textAlign="center"
-            color="cardinal"
-            text={`${prop('potency', product)} | ${prop('tension', product)}`}
-          />
-        </div>
-        <div className={styles.equipmentInfo}>
-          <img src={weightSvg} alt="weight product" />
-          <Title
-            size="small"
-            type="bold"
-            textAlign="center"
-            color="cardinal"
-            text={`${prop('weight', product)}`}
-          />
-        </div>
-      </div>
-      <div className={styles.revenuesTitle}>
+      <div className={styles.product}>
         <Title
           size="medium"
-          type="bold"
+          text={prop('name', product)}
+          textAlign="right"
+        />
+        <div className={styles.productImage}>
+          <img src={imagesProduct[prop('image', product)]} alt="product" />
+        </div>
+        <div className={styles.productContent}>
+          <Title
+            size="medium"
+            text={Translate('detail.description')}
+          />
+          <div className={styles.productInfo}>
+            <div className={styles.descriptionText}>
+              <Title
+                color="primary"
+                size="small"
+                text={prop('description', product)}
+              />
+            </div>
+            <div className={styles.productInfoSection}>
+              <Title
+                color="primary"
+                size="small"
+                text={Translate('detail.dimension')}
+              />
+              <Title
+                color="primary"
+                size="small"
+                type="bold"
+                text={`${Translate('detail.height')}: ${prop('height', product)} x ${Translate('detail.width')}: ${prop('width', product)} x ${Translate('detail.deep')}: ${prop('deep', product)}`}
+              />
+            </div>
+            <div className={styles.equipmentTechnical}>
+              <div className={styles.productInfoSection}>
+                <Title
+                  color="primary"
+                  size="small"
+                  text={Translate('detail.potencyTension')}
+                />
+                <Title
+                  color="primary"
+                  size="small"
+                  type="bold"
+                  text={`${prop('potency', product)} / ${prop('tension', product)}`}
+                />
+              </div>
+              <div className={styles.productInfoSection}>
+                <Title
+                  color="primary"
+                  size="small"
+                  text={Translate('detail.weight')}
+                />
+                <Title
+                  color="primary"
+                  size="small"
+                  type="bold"
+                  text={`${prop('weight', product)}`}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productRevenue}>
+        <Title
+          size="medium"
           text={Translate('detail.revenues')}
         />
       </div>
-      <div className={styles.revenuesContainer}>
-        { revenues && revenues.map(revenue)}
+      <div className={styles.productRevenueDocumet}>
+        <div className={styles.revenueGallery}>
+          {revenues.map(revenue)}
+        </div>
+        {
+          isOpen && (
+            <Lightbox
+              mainSrc={images[photoIndex]}
+              nextSrc={images[(photoIndex + 1) % images.length]}
+              prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+              onCloseRequest={() => setIsOpen(false)}
+              onMovePrevRequest={() =>
+                setPhotoIndex((photoIndex + images.length - 1) % images.length)
+              }
+              onMoveNextRequest={() =>
+                setPhotoIndex((photoIndex + 1) % images.length)
+              }
+            />
+          )
+        }
       </div>
-      <div className={styles.issuesTitle}>
-        <Title
-          size="medium"
-          type="bold"
-          text={Translate('detail.errors')}
-        />
-      </div>
-      <div className={styles.issuesContainer}>
-        {product.issues && map(issue, issues)}
-      </div>
-      {
-        isOpen && (
-          <Lightbox
-            mainSrc={images[photoIndex]}
-            nextSrc={images[(photoIndex + 1) % images.length]}
-            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-            onCloseRequest={() => setIsOpen(false)}
-            onMovePrevRequest={() =>
-              setPhotoIndex((photoIndex + images.length - 1) % images.length)
-            }
-            onMoveNextRequest={() =>
-              setPhotoIndex((photoIndex + 1) % images.length)
-            }
-          />
-        )
-      }
-      <Modal show={showModal}>
-        <div className={styles.headerModalIssue}>
-          <div
-            onClick={onCloseModal}
-            role="button"
-            className={styles.headerModalItem}
-          >
-            <img src={CloseSvg} alt="close modal" />
-          </div>
-          <div className={styles.headerModalItem}>
+      <div className={styles.productIssue}>
+      {product.issues && (
+          <div className={styles.productIssueTitle}>
             <Title
-              size="medium-1"
-              text={issueSelected.name || ''}
-              textAlign="center"
+              size="medium"
+              text={Translate('detail.errors')}
             />
           </div>
-        </div>
-        <div className={styles.modalContentIssue}>
-          <div className={styles.issueModalContent}>
-            <div className={styles.issueModalTitle}>
-              <Title
-                size="large"
-                text={Translate('detail.problem')}
-              />
-            </div>
-            {issueSelected.problems && issueSelected.problems.map((problem, index) => (
-              <Title
-                key={index}
-                color="secondary"
-                size="medium"
-                text={problem}
-              />
-            ))}
-          </div>
-          <div className={styles.issueModalContent}>
-            <div className={styles.issueModalTitle}>
-              <Title
-                size="large"
-                text={Translate('detail.solution')}
-              />
-            </div>
-            {issueSelected.resolves && issueSelected.resolves.map((resolve, index) => (
-              <Title
-                key={index}
-                color="secondary"
-                size="medium"
-                text={resolve}
-              />
-            ))}
-          </div>
-        </div>
-      </Modal>
+        )}
+        { product.issues && (
+           map(issue, prop('issues', product))
+        )}
+      </div>
     </div>
   )
 }
 
 Detail.propTypes = {
   goBack: PropTypes.func.isRequired,
-  issueSelected: PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    problems: PropTypes.arrayOf(
-      PropTypes.string,
-    ),
-    resolves: PropTypes.arrayOf(
-      PropTypes.string,
-    ),
-  }).isRequired,
-  onCloseModal: PropTypes.func.isRequired,
-  onOpenModal: PropTypes.func.isRequired,
   product: PropTypes.shape({}).isRequired,
-  showModal: PropTypes.bool.isRequired,
 }
 
 export default Detail
